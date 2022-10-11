0a41f4bc3f05c7c29425bcdd8d189da75f40d555
Eppu Hassinen <eppu.hassinen@tuni.fi>
2022-10-11 12:46:04 +0300

    commit 3

--- wk07/stl/stl3.cc
+++ wk07/stl/stl3.cc
@@ -14,6 +14,6 @@ using namespace std;
  */
 std::vector<int>::iterator findGivenValue(std::vector<int>& v, int given)
 {
-    return findGivenValue(v, given);
+    std::find(v.begin(), v.end(), given);
 }
 
