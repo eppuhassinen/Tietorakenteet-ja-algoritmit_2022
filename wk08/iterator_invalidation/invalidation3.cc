72c7c171ae31f37606c7fcb4738ca32318cdf4cd
Eppu Hassinen <eppu.hassinen@tuni.fi>
2022-10-25 14:44:32 +0300

    week 8 2nd commit

--- wk08/iterator_invalidation/invalidation3.cc
+++ wk08/iterator_invalidation/invalidation3.cc
@@ -8,17 +8,19 @@ using namespace std;
 /**
  * @brief duplicates even numbers in the vector, removes uneven numbers. Example: {1, 2, 3 4} -> {2, 2, 4, 4}
  *
- * @param vec vector to be handled
+ * @param v vector to be handled
  */
 void duplicateEvenRemoveUneven(std::vector<int>& v) {
 
     using Iter = std::vector<int>::iterator;
 
+    
+    v.reserve(v.size() * 2);
     Iter it = v.begin();
     Iter end = v.end();
-    v.reserve(v.size() * 2);
     while (it < end){
         if ( *it % 2 == 0 ){
+            
             v.insert(it, *it);
             it += 2;
             end++;
@@ -26,8 +28,9 @@ void duplicateEvenRemoveUneven(std::vector<int>& v) {
         else {
             v.erase(it);
             end--;
-        }
-    }
+        } 
+        
+    } 
     v.shrink_to_fit();
 }
 
