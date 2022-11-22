2260c232dcc7ca540f9ff1d53b01579821e4f80a
Eppu Hassinen <eppu.hassinen@tuni.fi>
2022-11-22 14:32:06 +0200

    mze3 jeejee 2

--- wk12/maze/maze3.cc
+++ wk12/maze/maze3.cc
@@ -7,11 +7,7 @@ using namespace std;
 
 // Remember to implement your own container here or in another cc file to store the nodes in the maze
 // If you define it elsewhere, remember to make sure it's accessible by this file as well.
-void connectt(Node& node1, Node& node2, string dir, string back_dir)
-{
-    node1.paths[dir] = &node2;
-    node2.paths[back_dir] = &node1;
-}
+
 
 /**
  * @brief Connect two adjacent nodes together. If the nodes are not adjacent, do nothing
@@ -22,29 +18,35 @@ void connectt(Node& node1, Node& node2, string dir, string back_dir)
 void connectNodes(Node& node1, Node& node2) {
     std::cout << "connectNodes not implemented" << std::endl;
     // Student code here to connect the nodes together
-    if ( (node1.loc.first - node2.loc.first) == 1 ) {
-        if ( node1.loc.second - node2.loc.second == 0 ) {
-            connectt(node1,node2,RIGHT,LEFT);
-        }
-    }
-    if ( std::abs(node1.loc.second - node2.loc.second) == 1 ) {
-        if ( node1.loc.first - node2.loc.first != 0 ) {
-            connectt(node1,node2,ABOVE,BELOW);
-        }
+    if ((std::abs(node1.loc.first - node2.loc.first) == 1 && node1.loc.second - node2.loc.second == 0)
+     || (std::abs(node1.loc.second - node2.loc.second) == 1  && node1.loc.first - node2.loc.first == 0) )
+
+    if (node1.loc.first < node2.loc.first)
+    {
+        node1.paths[RIGHT] = &node2;
+        node2.paths[LEFT] = &node1;
+        return;
     }
 
-    if ( (node2.loc.first - node1.loc.first) == -1 ) {
-        if ( node1.loc.second - node2.loc.second == 0 ) {
-            connectt(node1,node2,LEFT,RIGHT);
-        }
-    }
-    if ( std::abs(node2.loc.second - node1.loc.second) == -1 ) {
-        if ( node1.loc.first - node2.loc.first != 0 ) {
-            connectt(node1,node2,BELOW,ABOVE);
-        }
+    if (node1.loc.first > node2.loc.first)
+    {
+        node1.paths[LEFT] = &node2;
+        node2.paths[RIGHT] = &node1;
+        return;
     }
 
+    if (node1.loc.second < node2.loc.second)
+    {
+        node1.paths[ABOVE] = &node2;
+        node2.paths[BELOW] = &node1;
+        return;
+    }
 
+    if (node1.loc.second > node2.loc.second)
+    {
+        node1.paths[BELOW] = &node2;
+        node2.paths[ABOVE] = &node1;
+        return;
+    }
 
 }
-
